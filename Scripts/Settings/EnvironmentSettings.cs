5af3b902d62c57e7136332538b3f07da399968a6
benjaminmao123 <43651347+benjaminmao123@users.noreply.github.com>
2020-06-03 07:13:53 -0700

    Add files via upload

---
+++ Scripts/Settings/EnvironmentSettings.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bytes.Settings
+{
+    public static class EnvironmentSettings
+    {
+        public static float Gravity { get; set; } = 9.8f;
+    }
+}
---
+++ Scripts/Settings/PlayerSettings.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+public class PlayerSettings : MonoBehaviour
+{
+    public float sensitivity = 5;
+    public string playerName = "Player";
+    public float volume = 0.5f;
+    public string characterRoot = "Remy";
+    public string character = "RemyPlayer";
+
+    [SerializeField] Slider volumeSlider;
+    [SerializeField] Slider sensitivitySlider;
+
+    private void Start()
+    {
+        volumeSlider.value = volume;
+        sensitivitySlider.value = sensitivity;
+
+        if (PlayerPrefs.HasKey("sensitivity"))
+            volume = PlayerPrefs.GetFloat("sensitivity");
+        if (PlayerPrefs.HasKey("playerName"))
+            playerName = PlayerPrefs.GetString("playerName");
+        if (PlayerPrefs.HasKey("volume"))
+            volume = PlayerPrefs.GetFloat("volume");
+
+        PlayerPrefs.SetFloat("sensitivity", sensitivity);
+        PlayerPrefs.SetString("playerName", playerName);
+        PlayerPrefs.SetFloat("volume", volume);
+        PlayerPrefs.SetString("characterRoot", characterRoot);
+        PlayerPrefs.SetString("character", character);
+        PlayerPrefs.Save();
+    }
+
+    public void OnLeaveSettings()
+    {
+        volume = volumeSlider.value;
+        sensitivity = sensitivitySlider.value;
+
+        PlayerPrefs.SetFloat("sensitivity", sensitivity);
+        PlayerPrefs.SetString("playerName", playerName);
+        PlayerPrefs.SetFloat("volume", volume);
+        PlayerPrefs.SetString("characterRoot", characterRoot);
+        PlayerPrefs.SetString("character", character);
+        PlayerPrefs.Save();
+    }
+}
